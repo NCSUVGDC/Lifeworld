ffdda78dc052e790c8ef6dd3d149d53f9fec4d4f
DJ <dj@schilens.com>
2018-08-20 00:12:21 -0400

    Working demo of player with hands in a VR room
    Not much else of practical use but now's probably a good time to start
    using a `dev` branch!

---
+++ Source/Harlows_Wallpaper/Core/HarlowGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HarlowGameMode.generated.h"
+
+UCLASS()
+class HARLOWS_WALLPAPER_API AHarlowGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AHarlowGameMode();
+	
+};
