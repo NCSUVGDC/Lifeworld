a7e5615a8bb791b6c60516cb8cd148ea610968a4
DJ <dj@schilens.com>
2018-09-28 00:40:56 -0400

    Add HarlowPawn, HarlowGameState Core C++ Classes

---
+++ Source/Harlows_Wallpaper/Player/HarlowPawn.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HarlowPawn.h"
+
+
+// Sets default values
+AHarlowPawn::AHarlowPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AHarlowPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHarlowPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AHarlowPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
