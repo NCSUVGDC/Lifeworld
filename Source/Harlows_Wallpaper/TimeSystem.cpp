95ca879ce5b7b60399d7c015bde67db753ff7209
DJ <dj@schilens.com>
2018-10-03 13:25:08 -0400

    Committing a fix I need to get the Timesystem to compile
    I get errors that GEngine, etc. is an undeclared identifier. I believe this is related to me having to use a precompiled header hack to get UE4.20 to even compile on my machine. That being said this shouldn't break anyone else's setups if they already work.

--- Source/Harlows_Wallpaper/TimeSystem.cpp
+++ Source/Harlows_Wallpaper/TimeSystem.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TimeSystem.h"
-
+#include "Runtime/Engine/Classes/Engine/Engine.h"
 
 // Sets default values
 ATimeSystem::ATimeSystem()
